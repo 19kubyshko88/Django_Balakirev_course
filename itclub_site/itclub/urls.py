f7c8b05b5180a0fc67638e407696195719b27dfe
prospirity <19kubyshko88@gmail.com>
2023-10-05 15:51:03 +0300

    Динамические URL. Коныертеры int, slug. Пользовательские конвертеры.

--- itclub_site/itclub/urls.py
+++ itclub_site/itclub/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from itclub import views
+from django.urls import path, re_path, register_converter
+from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('',views.index), # http://127.0.0.1:8000/
-    path('groups/',views.groups), # http://127.0.0.1:8000/groups/
+    path('groups/<int:groups_id>/',views.groups), # http://127.0.0.1:8000/groups/number
+    path('groups/<slug:groups_slug>/',views.groups_by_slug), # http://127.0.0.1:8000/groups/numb-and-let
+    # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive)
+    path('archive/<year4:year>/', views.archive)
 ]
--- itclub_site/itclub/views.py
+++ itclub_site/itclub/views.py
@@ -2,9 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request): # request  это HttpRequest
+def index(request):  # request  это HttpRequest
     return HttpResponse("Страница ITclub")
 
 
-def groups(request): # request  это HttpRequest
-    return HttpResponse( "<h1>Группы</h1>")
+def groups(request, groups_id):  # request  это HttpRequest
+    return HttpResponse(f"<h1>Группы</h1><p>ID: {groups_id}</p>")
+
+
+def groups_by_slug(request, groups_slug):  # request  это HttpRequest
+    return HttpResponse(f"<h1>Группы</h1><p>Slug: {groups_slug}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
