21d6df9c72bab097abfbebd106d5152779d6e6ec
19kubyshko88 <19kubyshko88@gmail.com>
2023-10-12 13:27:20 +0300

    #8. Перенаправления (redirect). Функция reverse GET- и POST-запросы.
    ВАЖНО! При перенаправлениях возможно страница подтягивается из кэша, если до этого была загружена. Удали кэш или выбери другой год в archive/year

--- itclub_site/itclub/urls.py
+++ itclub_site/itclub/urls.py
@@ -5,9 +5,9 @@ from . import converters
 register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
-    path('', views.index),  # http://127.0.0.1:8000/
-    path('groups/<int:groups_id>/', views.groups),  # http://127.0.0.1:8000/groups/number
-    path('groups/<slug:groups_slug>/', views.groups_by_slug),  # http://127.0.0.1:8000/groups/numb-and-let
+    path('', views.index, name='home'),  # http://127.0.0.1:8000/
+    path('groups/<int:groups_id>/', views.groups, name='groups_id'),  # http://127.0.0.1:8000/groups/number
+    path('groups/<slug:groups_slug>/', views.groups_by_slug, name='groups_slug'),  # http://127.0.0.1:8000/groups/numb-and-let
     # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive)
-    path('archive/<year4:year>/', views.archive)
+    path('archive/<year4:year>/', views.archive, name='archive')
 ]
--- itclub_site/itclub/views.py
+++ itclub_site/itclub/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def index(request):  # request  это HttpRequest
@@ -11,13 +12,24 @@ def groups(request, groups_id):  # request  это HttpRequest
 
 
 def groups_by_slug(request, groups_slug):  # request  это HttpRequest
+    if request.GET:         # http://127.0.0.1:8000/groups/3d/?day=tuesday&class=1
+        print(request.GET)   # То же request.POST
     return HttpResponse(f"<h1>Группы</h1><p>Slug: {groups_slug}</p>")
 
 
 def archive(request, year):
+    print(year, type(year))
     if year > 2023:
-        raise Http404()
-
+        # raise Http404()
+        # return redirect('/', permanent=True)
+        # return redirect(index)
+        # return redirect('home')
+        # return redirect('groups_slug', '3D')
+        # uri = reverse('groups', args=('3D', ))
+        # return redirect(uri)
+        # return HttpResponseRedirect('/')
+        uri = reverse('groups_slug', args=('Scratch', ))
+        return HttpResponsePermanentRedirect(uri)
     return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
 
 
