98d69ca945cdc68147739ec9f76b74a3f24f164f
19kubyshko88 <19kubyshko88@gmail.com>
2023-10-19 13:20:19 +0300

    chore: DEBUG=True

--- itclub_site/itclub_site/settings.py
+++ itclub_site/itclub_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-shl1$)w_&u#3j)qxc)y^t@0ul=3-_cpw1*=nh_3tgn75y67pk8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
