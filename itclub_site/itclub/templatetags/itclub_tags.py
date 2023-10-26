6b7847e146e6adf7a457b88859c7280689425450
19kubyshko88 <19kubyshko88@gmail.com>
2023-10-26 14:01:49 +0300

    #16. Пользовательские теги шаблонов. Декоратор simple_tag. Псевдоним для simple_tag

--- itclub_site/itclub/templatetags/itclub_tags.py
+++ itclub_site/itclub/templatetags/itclub_tags.py
@@ -4,6 +4,6 @@ import itclub.views as views
 register = template.Library() # для регистрации новых тегов
 
 
-@register.simple_tag()
+@register.simple_tag(name='getcats')
 def get_categories():
     return views.cats_db
