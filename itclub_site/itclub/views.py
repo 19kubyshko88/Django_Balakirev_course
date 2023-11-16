062b20a372c04a340539684358723d5450aa8b2e
19kubyshko88 <19kubyshko88@gmail.com>
2023-11-16 05:44:32 +0300

    #22. Слаги (slug) в URL-адресах.
    slug – это уникальный фрагмент URL-адреса, ассоциированный с конкретной записью и, обычно, состоит из набора маленьких латинских букв, цифр, символов подчеркивания и дефиса.
    
    Вначале сделали отображение статей по их идентификатору

--- itclub_site/itclub/views.py
+++ itclub_site/itclub/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,  get_object_or_404
 from django.template.loader import render_to_string
 from django.urls import reverse
 from django.template.defaultfilters import slugify
+
+from .models import StudentArticles
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
@@ -47,7 +49,16 @@ def about(request):
 
 
 def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+    post = get_object_or_404(StudentArticles, pk=post_id)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+
+    return render(request, 'itclub/post.html', context=data)
 
 
 def addpage(request):
