21d6787764b65ef3b394760b1b1bd11491c9f43d
19kubyshko88 <19kubyshko88@gmail.com>
2023-11-23 11:18:10 +0300

    #23. Создание пользовательского менеджера модели. Перечисляемое поле
    is_published = 1 или 0. Заменим цифры на осмысленных имена - перечисления.
    https://docs.djangoproject.com/en/4.2/ref/models/fields/#enumeration-types
    IntegerChoices – для числовых наборов;
    TextChoices – для строковых наборов.
    
    Атрибуты  DRAFT и PUBLISHED должны быть описаны как кортежи, состоящие из двух элементов: первое значение – это то, которое записывается в БД, а второе – это его имя (метка). В частности, имена используются в админ-панели Django.
    
    нужно создать и применить миграцию для изменения таблицы в БД:
    python manage.py makemigrations
    python manage.py migrate
    
    Как это все будет работать:
    python manage.py shell_plus --print-sql
    a = StudentArticles.objects.all()
    a.update(is_published=StudentArticles.Status.DRAFT)
    a.update(is_published=StudentArticles.Status.PUBLISHED)
    StudentArticles.Status.choices
    StudentArticles.Status.values
    StudentArticles.Status.labels

--- itclub_site/itclub/models.py
+++ itclub_site/itclub/models.py
@@ -4,16 +4,19 @@ from django.urls import reverse
 
 class PublishedModel(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(is_published=1)
+        return super().get_queryset().filter(is_published=StudentArticles.Status.PUBLISHED)
 
 
 class StudentArticles(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, db_index=True, unique=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
 
     published = PublishedModel()
     objects = models.Manager()  # после published objects надо переопределять, иначе не будет такого поля.
