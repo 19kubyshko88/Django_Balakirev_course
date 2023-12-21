a536eb70666117b01ebdd7fe476166010047a0cd
prospirity <19kubyshko88@gmail.com>
2023-12-21 19:18:56 +0300

    #39. Пользовательские поля и действия в админ-панели.
    Добавили поле с количеством слов в статье.

--- itclub_site/itclub/admin.py
+++ itclub_site/itclub/admin.py
@@ -4,11 +4,21 @@ from .models import StudentArticles, Category
 
 @admin.register(StudentArticles)
 class ArticlesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published')
+    list_display = ('id', 'title', 'time_create', 'is_published', 'brief_info')
     list_display_links = ('id', 'title')
     ordering = ['-time_create', 'title']
     list_editable = ('is_published',)
     list_per_page = 3
+
+    @staticmethod
+    def brief_info(st_article: StudentArticles):
+        """
+        Метод возвращает количество слов в статье.
+        :param st_article:
+        :return:
+        """
+        content: str = st_article.content
+        return f"Количество слов: {len(content.split())}"
 
 
 @admin.register(Category)
