301266ecaa038c1bb004f6a017c2f28f33e2f124
19kubyshko88 <19kubyshko88@gmail.com>
2023-12-21 14:10:38 +0300

    #38. Настройка отображения списка статей в админ-панели
    Сортировка для админ-панели (на сортировку в запросах не влияет).

--- itclub_site/itclub/admin.py
+++ itclub_site/itclub/admin.py
@@ -6,6 +6,6 @@ from .models import StudentArticles
 class ArticlesAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'is_published')
     list_display_links = ('id', 'title')
-
+    ordering = ['-time_create', 'title']
 
 # admin.site.register(StudentArticles, ArticlesAdmin)
