5da5617eb188e5b1e7e61cd3d0b1f62069bf81a2
19kubyshko88 <19kubyshko88@gmail.com>
2023-12-21 14:07:28 +0300

    #38. Настройка отображения списка статей в админ-панели
    То же, но с помощью декоратора.

--- itclub_site/itclub/admin.py
+++ itclub_site/itclub/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from .models import StudentArticles
 
 
+@admin.register(StudentArticles)
 class ArticlesAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'is_published')
     list_display_links = ('id', 'title')
 
 
-admin.site.register(StudentArticles, ArticlesAdmin)
+# admin.site.register(StudentArticles, ArticlesAdmin)
