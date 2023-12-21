9f5d708885864974594ed18dda0b6635491bd834
prospirity <19kubyshko88@gmail.com>
2023-12-21 19:23:38 +0300

    #39. Пользовательские поля и действия в админ-панели.
    Убрали id, переименовали поле brief_info.

--- itclub_site/itclub/admin.py
+++ itclub_site/itclub/admin.py
@@ -4,13 +4,14 @@ from .models import StudentArticles, Category
 
 @admin.register(StudentArticles)
 class ArticlesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'time_create', 'is_published', 'brief_info')
-    list_display_links = ('id', 'title')
+    list_display = ('title', 'time_create', 'is_published', 'brief_info')
+    list_display_links = ('title',)
     ordering = ['-time_create', 'title']
     list_editable = ('is_published',)
     list_per_page = 3
 
     @staticmethod
+    @admin.display(description="Кол-во слов")
     def brief_info(st_article: StudentArticles):
         """
         Метод возвращает количество слов в статье.
