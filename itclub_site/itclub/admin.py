6c8230697a8218da4a097c8a55e60ffd29b6ff16
19kubyshko88 <19kubyshko88@gmail.com>
2023-12-21 13:59:52 +0300

    #38. Настройка отображения списка статей в админ-панели
    добавим в список записей дополнительные поля: id, title, время создания и флаг публикации.
    
    list_display_links = ('id', 'title') - чтобы сслыкой были и названия статей, а не только индексы.

--- itclub_site/itclub/admin.py
+++ itclub_site/itclub/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import StudentArticles
 
-admin.site.register(StudentArticles)
+
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'is_published')
+    list_display_links = ('id', 'title')
+
+
+admin.site.register(StudentArticles, ArticlesAdmin)
