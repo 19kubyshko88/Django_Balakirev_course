048fee208bb6cb16855a78682a6b35982c8bdbce
19kubyshko88 <19kubyshko88@gmail.com>
2023-12-26 00:09:04 +0300

    #41. Настройка формы редактирования записей в админ-панели. Поле только для чтения.
    Если оставить так, то можно будет создать только еще одну запись. Т.к. слаг будет пустым, а они должны быть уникальными. Поэтому нужно сделать автоматическое формирование слага.

--- itclub_site/itclub/admin.py
+++ itclub_site/itclub/admin.py
@@ -23,8 +23,9 @@ class LongArticleFilter(admin.SimpleListFilter):
 
 @admin.register(StudentArticles)
 class ArticlesAdmin(admin.ModelAdmin):
-    # fields = ['title', 'content', 'slug','cat'] # Если не указать cat, то будет ошибка, т.к. обязательное поле
-    exclude = ['tags', 'is_published']
+    fields = ['title', 'content', 'slug','cat'] # Если не указать cat, то будет ошибка, т.к. обязательное поле
+    # exclude = ['tags', 'is_published']
+    readonly_fields = ['slug']
     list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
     list_display_links = ('title',)
     ordering = ['-time_create', 'title']
