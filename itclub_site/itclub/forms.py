1c3c440d05cdac5a66c446ccf400cd237005ff5e
19kubyshko88 <19kubyshko88@gmail.com>
2023-12-28 11:59:38 +0300

    #45. Отображение полей формы. Сохранение переданных данных в БД
    form - итерируемы объект. В {% for f in form %} f - возвращает form.slug, form.content, form.is_published и т.д.
    
     for="{{ f.id_for_label }}" нужен для привязки label к полю формы. Если его указать, то при клике на label, фокус перейдёт на соответствующее поле ввода.
    
     В forms.py через параметр widget и attrs можно задать отдельные стили вместо указанного в addpage.html  class="form-label"

--- itclub_site/itclub/forms.py
+++ itclub_site/itclub/forms.py
@@ -6,9 +6,9 @@ class AddPostForm(forms.Form):
     """
     класс, описывающий форму добавления статьи
     """
-    title = forms.CharField(max_length=255, label="Заголовок")
+    title = forms.CharField(max_length=255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))
     slug = forms.SlugField(max_length=255, label="URL")
-    content = forms.CharField(widget=forms.Textarea(), required=False, label="Контент")
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент")
     is_published = forms.BooleanField(required=False, label="Статус")
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
     summary = forms.CharField(widget=forms.Textarea(), required=False, label="Резюме")
