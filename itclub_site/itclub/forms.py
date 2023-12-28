ab815ca036097a0a174f3548f886611b664bf6d2
19kubyshko88 <19kubyshko88@gmail.com>
2023-12-28 11:21:12 +0300

    #45. Отображение полей формы. Сохранение переданных данных в БД
    Поменяли названия полей формы.

--- itclub_site/itclub/forms.py
+++ itclub_site/itclub/forms.py
@@ -6,9 +6,9 @@ class AddPostForm(forms.Form):
     """
     класс, описывающий форму добавления статьи
     """
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    summary = forms.CharField(widget=forms.Textarea(), required=False)
+    title = forms.CharField(max_length=255, label="Заголовок")
+    slug = forms.SlugField(max_length=255, label="URL")
+    content = forms.CharField(widget=forms.Textarea(), required=False, label="Контент")
+    is_published = forms.BooleanField(required=False, label="Статус")
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории")
+    summary = forms.CharField(widget=forms.Textarea(), required=False, label="Резюме")
