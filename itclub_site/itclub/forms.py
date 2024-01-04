1c6fa83c1b455789ead1f4e72fbdc089825a0589
19kubyshko88 <19kubyshko88@gmail.com>
2024-01-04 17:06:01 +0300

    #46. Валидация полей формы.
    Каждый класс поля формы и модели имеет атрибут validators, позволяющий указывать список классов-валидаторов для предобработки переданных данных на стороне сервера.
    
    В классе модели StudentArticles для поля slug указать такие же валидаторы. Они будут использоваться при проверке перед записью данных в БД.

--- itclub_site/itclub/forms.py
+++ itclub_site/itclub/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Category, Summary
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 
 class AddPostForm(forms.Form):
@@ -12,7 +13,10 @@ class AddPostForm(forms.Form):
                                 'min_length': 'Слишком короткий заголовок',
                                 'required': 'Без заголовка - никак',
                             })
-    slug = forms.SlugField(max_length=255, label="URL")
+    slug = forms.SlugField(max_length=255, label="URL", validators=[
+        MinLengthValidator(5, message="Минимум 5 символов"),
+        MaxLengthValidator(100, message="Максимум 100 символов"),
+    ])
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Контент")
     is_published = forms.BooleanField(required=False, label="Статус")
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
--- itclub_site/itclub/models.py
+++ itclub_site/itclub/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 # from django.template.defaultfilters import slugify
 import transliterate
 
@@ -14,7 +15,10 @@ class StudentArticles(models.Model):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
     title = models.CharField(max_length=255, verbose_name="Заголовок")
-    slug = models.SlugField(max_length=255, db_index=True, unique=True)
+    slug = models.SlugField(max_length=255, db_index=True, unique=True, validators=[
+                                                                                MinLengthValidator(5),
+                                                                                MaxLengthValidator(100),
+                                                                            ])
     content = models.TextField(blank=True, verbose_name="Текст статьи")
     time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
