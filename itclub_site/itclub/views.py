96b48de0c4c6e66fbe7cd1449d27be78dc898406
19kubyshko88 <19kubyshko88@gmail.com>
2024-01-05 23:22:28 +0300

    #48. Загрузка (upload) файлов на сервер. Любых.
    about.html
    без enctype="multipart/form-data" ошибки не выдаст, но без него файлы загружаться на сервер не будут. Также должно быть определено специальное поле input с типом file и стандартная кнопка формы для отправки данных. Это минимальный набор элементов для выбора и отправки произвольного файла на сервер.
    
    views.py
    https://docs.djangoproject.com/en/4.2/topics/http/file-uploads/
    request.FILES['file_upload'] появился, п.ч. в about.html указали name='file_upload'
    
    Загружаются любые файлы. Если просто нажать отправить, то будет ошибка.

--- itclub_site/itclub/views.py
+++ itclub_site/itclub/views.py
@@ -26,8 +26,17 @@ def index(request):
     return render(request, 'itclub/index.html', context=data)
 
 
+def handle_uploaded_file(f):
+    with open(f"uploads/{f.name}", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
 def about(request):
     data = {'title': "О сайте ITclub"}
+    if request.method == "POST":
+        handle_uploaded_file(request.FILES['file_upload'])
+
     return render(request, 'itclub/about.html', {'title':'О сайте', 'menu': menu})
 
 
