d98936b9e4162edfd2e41df4091d7d497e0653de
19kubyshko88 <19kubyshko88@gmail.com>
2024-01-05 18:39:42 +0300

    #47. Формы связанные с моделями. Добавление поля summary_text.
    summary_text, а не summary, п.ч. в StudentArticles есть поле summary. Метод save нужен, чтобы перед созданием записи в StudentArticles, создать азпись в Summary.

--- itclub_site/itclub/forms.py
+++ itclub_site/itclub/forms.py
@@ -23,12 +23,20 @@ class AddPostForm(forms.ModelForm):
     класс, описывающий форму добавления статьи
     """
     cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
+    summary_text = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}),  required=False, label="Резюме")
 
     class Meta:
         model = StudentArticles
-        fields = ['title', 'slug', 'content', 'is_published', 'cat','tags']
+        fields = ['title', 'slug', 'content', 'is_published', 'cat', 'tags']
         labels = {'slug': 'URL'}
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-input'}), # css стиль
             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
         }
+
+    def save(self, commit=True):
+        article = super().save(commit=False)
+        article.summary= Summary.objects.create(summary_text=self.cleaned_data['summary_text'])
+        if commit:
+            article.save()
+        return article
