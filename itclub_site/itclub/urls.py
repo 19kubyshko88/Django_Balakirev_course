a31929dfe0bae3f344a871b6affaaf42e5d8d673
19kubyshko88 <19kubyshko88@gmail.com>
2024-01-08 23:36:00 +0300

    clear: уберем лишние строки.

--- itclub_site/itclub/urls.py
+++ itclub_site/itclub/urls.py
@@ -16,8 +16,4 @@ urlpatterns = [
     path('post/<slug:post_slug>/', views.ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>/', views.ArticlesByCategory.as_view(), name='category'),
     path('tag/<slug:tag_slug>/', views.ArticlesByTag.as_view(), name='tag'),
-    path('groups/<int:groups_id>/', views.groups, name='groups_id'),  # http://127.0.0.1:8000/groups/number
-    path('groups/<slug:groups_slug>/', views.groups_by_slug, name='groups_slug'),  # http://127.0.0.1:8000/groups/numb-and-let
-    # re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive)
-    path('archive/<year4:year>/', views.archive, name='archive'),
 ]
