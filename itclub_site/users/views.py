e717eff8253b51ba476726a48b5c4de8248783c5
19kubyshko88 <19kubyshko88@gmail.com>
2024-01-10 00:28:45 +0300

    #62. Класс LoginView
    users/views.py-> users/urls.py-> users/templates/users/login.html-> см.ниже
    
    LoginView – класс представления для авторизации пользователей;
    
    -> settings.py
    При вводе логина и пароля происходит автоматическое перенаправление в профайл по URL-адресу: http://127.0.0.1:8000/accounts/profile/
     Изменим адрес перенаправления в файле settings
    
    LOGIN_REDIRECT_URL – задает URL-адрес, на который следует перенаправлять пользователя после успешной авторизации;

--- itclub_site/users/views.py
+++ itclub_site/users/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 
 
-def login_user(request):
-    return HttpResponse("login")
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'users/login.html'
+    extra_context = {'title': "Авторизация"}
+
+    # def get_success_url(self):  # можно использовать для изменения автоматического перенаправление в профайл
+    #     return reverse_lazy('home')
 
 
 def logout_user(request):
