a427b4ccdb001f5536522023c89e75b8883bab42
19kubyshko88 <19kubyshko88@gmail.com>
2024-01-15 11:53:16 +0300

    #66. Профайл пользователя.
    urls.py-> users/views.py
    
     описанный ранее маршрут users/profile/id позволяет просматривать и редактировать любые профайлы. Поэтому мы уберем отбор записей из таблицы user по идентификатору.
     Теперь профайл будет открываться только для текущего пользователя, либо сделано перенаправление на страницу авторизации для неавторизованных пользователей.

--- itclub_site/users/urls.py
+++ itclub_site/users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterUser.as_view(), name='register'),
-    path('profile/<int:pk>/', views.ProfileUser.as_view(), name='profile'),
+    path('profile/', views.ProfileUser.as_view(), name='profile'),
 ]
